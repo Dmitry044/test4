215d141584d3700abe11b63eda52a0f1dce9d7cc
Dmitry Avdeenko <joh@ex.com>
2021-06-21 20:06:27 +0200

    Perviy commit

---
+++ GitClass.java
@@ -0,0 +1,11 @@
+package GitSummer;
+
+public class GitClass {
+    private static Object myObj;
+    int x = 5;
+
+    public static void main (String[] args) {
+        GitClass mygit = new GitClass();
+        System.out.print(myObj.x);
+    }
+}
